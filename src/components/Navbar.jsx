feec12003632f26a0ab0f5f86dcb71cead928ce9
Atulbedwal <atulbedwal69@gmail.com>
2023-07-04 01:28:56 +0530

    commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { Stack, Typography } from "@mui/material";
+import { fontSize } from "@mui/system";
+import { Link } from "react-router-dom";
+
+import { logo } from "../utils/constants";
+import { SearchBar } from "./";
+
+const Navbar = () => (
+  <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
+    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+      <img src={logo} alt="logo" height={45} />
+      <Typography variant="h6" component="h2" sx={{ color: "white", fontWeight: "bold", ml: 1.5 }}>
+        <span style={{ color: "white" ,fontSize:"3vh"}}>Lame</span>
+        <span style={{ color: "red",fontSize:"3.2vh" }}>Tube</span>
+      </Typography>
+    </Link>
+    <SearchBar />
+  </Stack>
+);
+
+export default Navbar;
